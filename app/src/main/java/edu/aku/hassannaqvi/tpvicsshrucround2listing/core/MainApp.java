e85de6a73b57972d42f3b4bbf7426266218fca98
Ibrahimarain <ibrahimarain15@gmail.com>
2022-06-17 16:58:33 +0500

    Initial commit

---
+++ app/src/main/java/edu/aku/hassannaqvi/tpvicsshrucround2listing/core/MainApp.java
@@ -0,0 +1,346 @@
+package edu.aku.hassannaqvi.tpvicsshrucround2listing.core;
+
+import static edu.aku.hassannaqvi.tpvicsshrucround2listing.database.CreateTable.DATABASE_NAME;
+import static edu.aku.hassannaqvi.tpvicsshrucround2listing.database.DatabaseHelper.DATABASE_PASSWORD;
+
+import android.Manifest;
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+
+import androidx.core.app.ActivityCompat;
+
+import com.edittextpicker.aliazaz.EditTextPicker;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.validatorcrawler.aliazaz.Clear;
+
+import net.sqlcipher.database.SQLiteDatabase;
+
+import org.json.JSONArray;
+
+import java.io.File;
+import java.util.List;
+
+import edu.aku.hassannaqvi.tpvicsshrucround2listing.BuildConfig;
+import edu.aku.hassannaqvi.tpvicsshrucround2listing.R;
+import edu.aku.hassannaqvi.tpvicsshrucround2listing.models.Cluster;
+import edu.aku.hassannaqvi.tpvicsshrucround2listing.models.Listings;
+import edu.aku.hassannaqvi.tpvicsshrucround2listing.models.Mwra;
+import edu.aku.hassannaqvi.tpvicsshrucround2listing.models.Users;
+
+
+public class MainApp extends Application {
+
+    public static final String PROJECT_NAME = "tpvicsshrucround2listing";
+    public static final String DIST_ID = null;
+    public static final String SYNC_LOGIN = "sync_login";
+    public static final String _IP = "https://vcoe1.aku.edu";// .LIVE server
+    // public static final String _IP = "http://f49461:8080/prosystem";// .TEST server
+    //public static final String _IP = "http://43.245.131.159:8080";// .TEST server
+    public static final String _HOST_URL = MainApp._IP + "/tpvics_shruc_r2/api/";// .TEST server;
+    public static final String _SERVER_URL = "syncGCM.php";
+    public static final String _SERVER_GET_URL = "getDataGCM.php";
+    public static final String _PHOTO_UPLOAD_URL = _HOST_URL + "uploads.php";
+    public static final String _UPDATE_URL = MainApp._IP + "/tpvics_shruc_r2/app/listing";
+    public static final String _APP_FOLDER = "../app/listing";
+    public static final String _EMPTY_ = "";
+    public static final String _USER_URL = "resetpassword.php";
+    private static final String TAG = "MainApp";
+    public static int TRATS = 8;
+    public static String IBAHC = "";
+
+    //COUNTRIES
+    public static int PAKISTAN = 1;
+    public static int TAJIKISTAN = 3;
+
+    public static File sdDir;
+    public static String[] downloadData;
+
+    // Tables
+    public static Listings listings;
+    public static Mwra mwra;
+
+
+    public static String DeviceURL = "devices.php";
+    public static AppInfo appInfo;
+    public static Users user;
+    public static Boolean admin = false;
+    public static List<JSONArray> uploadData;
+    public static SharedPreferences.Editor editor;
+    public static SharedPreferences sharedPref;
+    public static String deviceid;
+    public static int versionCode = BuildConfig.VERSION_CODE;
+    public static String versionName = BuildConfig.VERSION_NAME;
+    public static int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE = 2;
+    public static long TWO_MINUTES = 1000 * 60 * 2;
+    public static boolean permissionCheck = false;
+    private static final long MINIMUM_DISTANCE_CHANGE_FOR_UPDATES = 1; // in Meters
+    private static final long MINIMUM_TIME_BETWEEN_UPDATES = 1000; // in Milliseconds
+    protected static LocationManager locationManager;
+    public static int idType = 0;
+    public static boolean mwraComplete;
+    public static boolean childComplete;
+    public static boolean pregComplete;
+    public static int mwraCount = 0;
+    public static int childCount = 0;
+    public static int pregCount = 0;
+    public static String selectedFemale = "";
+    public static String selectedChild = "";
+    public static String selectedPreg = "";
+    public static int mwraCountComplete = 0;
+    public static int childCountComplete = 0;
+    public static int pregCountComplete = 0;
+    public static List<String> subjectNames;
+    public static Cluster selectedCluster;
+    public static int maxStructure;
+    public static int hhid;
+    public static int HHCount = 0;
+    public static String[] clusterInfo;
+    public static String selectedTab;
+
+    private LocationCallback locationCallback;
+    LocationRequest mLocationRequest;
+
+    private static final long INTERVAL = 1000 * 10;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+
+
+    public static void hideSystemUI(View decorView) {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    public static String getDeviceId(Context context) {
+        String deviceId;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            deviceId = Settings.Secure.getString(
+                    context.getContentResolver(),
+                    Settings.Secure.ANDROID_ID);
+        } else {
+           /* final TelephonyManager mTelephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            if (mTelephony.getDeviceId() != null) {
+                deviceId = mTelephony.getDeviceId();
+            } else {
+                deviceId = Settings.Secure.getString(
+                        context.getContentResolver(),
+                        Settings.Secure.ANDROID_ID);
+            }*/
+        }
+        return "deviceId";
+    }
+
+    public static void cbCheck(CheckBox cb1, CheckBox cb2, CheckBox cb3, EditTextPicker edt) {
+        cb1.setOnCheckedChangeListener((compoundButton, b) -> {
+            if (b) {
+                Clear.clearAllFields(edt, false);
+                cb2.setChecked(false);
+                cb2.setEnabled(false);
+                cb3.setChecked(false);
+                cb3.setEnabled(false);
+            } else {
+                Clear.clearAllFields(edt, true);
+                cb2.setEnabled(true);
+                cb3.setEnabled(true);
+            }
+        });
+    }
+
+    public static Boolean isNetworkAvailable(Context c) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) c.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Network nw = connectivityManager.getActiveNetwork();
+            if (nw == null) return false;
+            NetworkCapabilities actNw = connectivityManager.getNetworkCapabilities(nw);
+            return actNw != null && (actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) || actNw.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH));
+        } else {
+            NetworkInfo nwInfo = connectivityManager.getActiveNetworkInfo();
+            return nwInfo != null && nwInfo.isConnected();
+        }
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        /*
+        RootBeer rootBeer = new RootBeer(this);
+        if (rootBeer.isRooted()) {
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        }*/
+
+        //Initiate DateTime
+        //Initializ App info
+        appInfo = new AppInfo(this);
+        sharedPref = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+        editor = sharedPref.edit();
+        MainApp.editor
+                .putString("mh01", "")
+                .apply();
+        deviceid = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                MINIMUM_TIME_BETWEEN_UPDATES,
+                MINIMUM_DISTANCE_CHANGE_FOR_UPDATES,
+                new GPSLocationListener(this) // Implement this class from code
+
+        );
+
+//        if (checkPlayServices(this)) {
+//            createLocationRequest();
+//            locationCallback = new LocationCallback() {
+//                @Override
+//                public void onLocationResult(LocationResult locationResult) {
+//                    if (locationResult == null) {
+//                        return;
+//                    }
+//                    for (Location location : locationResult.getLocations()) {
+//                        Log.i(TAG, "onLocationResult: "+ location);
+//                        onLocationChanged(location);
+//
+//                    }
+//                }
+//            };
+//
+//            getLocationUpdates(this, mLocationRequest, locationCallback);
+//
+//        }
+        initSecure();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+    }
+
+    public void onLocationChanged(Location location) {
+
+        SharedPreferences sharedPref = getSharedPreferences("GPSCoordinates", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        String dt = DateFormat.format("dd-MM-yyyy HH:mm", Long.parseLong(sharedPref.getString("Time", "0"))).toString();
+
+        Location bestLocation = new Location("storedProvider");
+        bestLocation.setAccuracy(Float.parseFloat(sharedPref.getString("Accuracy", "0")));
+        bestLocation.setTime(Long.parseLong(sharedPref.getString(dt, "0")));
+        bestLocation.setLatitude(Float.parseFloat(sharedPref.getString("Latitude", "0")));
+        bestLocation.setLongitude(Float.parseFloat(sharedPref.getString("Longitude", "0")));
+
+
+        if (isBetterLocation(location, bestLocation)) {
+            editor.putString("Longitude", String.valueOf(location.getLongitude()));
+            editor.putString("Latitude", String.valueOf(location.getLatitude()));
+            editor.putString("Accuracy", String.valueOf(location.getAccuracy()));
+            editor.putString("Time", String.valueOf(location.getTime()));
+            String date = DateFormat.format("dd-MM-yyyy HH:mm", Long.parseLong(String.valueOf(location.getTime()))).toString();
+
+
+            editor.apply();
+        }
+    }
+
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            return true;
+        }
+
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+
+        if (isSignificantlyNewer) {
+            return true;
+
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        boolean isFromSameProvider = isSameProvider(location.getProvider(),
+                currentBestLocation.getProvider());
+
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else return isNewer && !isSignificantlyLessAccurate && isFromSameProvider;
+    }
+
+    private boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+            return provider2 == null;
+        }
+        return provider1.equals(provider2);
+    }
+
+    private void initSecure() {
+        // Initialize SQLCipher library
+        SQLiteDatabase.loadLibs(this);
+        File databaseFile = getDatabasePath(DATABASE_NAME);
+       /* databaseFile.mkdirs();
+        databaseFile.delete();*/
+        SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(databaseFile, DATABASE_PASSWORD, null);
+        // Prepare encryption KEY
+        ApplicationInfo ai = null;
+        try {
+            ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            Bundle bundle = ai.metaData;
+            TRATS = bundle.getInt("YEK_TRATS");
+            //IBAHC = bundle.getString("YEK_REVRES").substring(TRATS, TRATS + 16);
+            IBAHC = bundle.getString("YEK_REVRES");
+            Log.d(TAG, "onCreate: YEK_REVRES = " + IBAHC);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/tpvicsshrucround2listing/utils/AndroidUtility.kt
@@ -0,0 +1,124 @@
+package edu.aku.hassannaqvi.tpvicsshrucround2listing.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
+import android.location.LocationRequest
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.os.Looper
+import android.provider.Settings
+import android.telephony.TelephonyManager
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.common.ConnectionResult
+
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.tasks.CancellationToken
+import com.google.android.gms.tasks.OnTokenCanceledListener
+import com.google.android.gms.tasks.Task
+
+
+fun isNetworkConnected(context: Context): Boolean {
+    var result = false
+    val connMgr =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val networkCapabilities = connMgr.activeNetwork ?: return false
+        val networkInfo = connMgr.getNetworkCapabilities(networkCapabilities) ?: return false
+        result = when {
+            networkInfo.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            networkInfo.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            networkInfo.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    } else {
+        connMgr.run {
+            connMgr.activeNetworkInfo?.run {
+                result = when (type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+            }
+        }
+    }
+
+    return result
+}
+
+@SuppressLint("HardwareIds")
+fun getIMEIInfo(context: Context): String {
+
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        Settings.Secure.getString(
+            context.contentResolver,
+            Settings.Secure.ANDROID_ID
+        )
+    } else {
+        val mTelephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        mTelephony.imei
+    }
+}
+
+
+fun getCurrentDeviceLocation(context: Context) : Task<Location> {
+
+     var fusedLocationClient: FusedLocationProviderClient =
+         LocationServices.getFusedLocationProviderClient(context)
+
+    val cancellationToken : CancellationToken = object : CancellationToken(){
+        override fun onCanceledRequested(onTokenCanceledListener: OnTokenCanceledListener): CancellationToken {
+            return this
+        }
+        override fun isCancellationRequested(): Boolean {
+            return false
+        }
+    }
+    return fusedLocationClient.getCurrentLocation(LocationRequest.QUALITY_BALANCED_POWER_ACCURACY, cancellationToken)
+}
+
+fun getLocationUpdates(context: Context, locationRequest:com.google.android.gms.location.LocationRequest, locationCallback: LocationCallback){
+
+    var fusedLocationClient: FusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(context)
+
+
+    fusedLocationClient.requestLocationUpdates(locationRequest,
+        locationCallback,
+        Looper.getMainLooper())
+
+}
+
+
+
+fun checkPlayServices(context: Context): Boolean {
+    val googleAPI = GoogleApiAvailability.getInstance()
+    val result = googleAPI.isGooglePlayServicesAvailable(context)
+    if (result != ConnectionResult.SUCCESS) {
+        return false
+    }
+    return true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
